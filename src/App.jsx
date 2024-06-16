902981f5f53ab86f1efe531bd9b663ae1a5ec44e
rizanoor <rizanoorfauzan123@gmail.com>
2024-06-16 08:53:14 +0700

    feat: slicing footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/organisems/Navbar'
+import Footer from './components/organisems/Footer'
 
 function App() {
 
   return (
     <>
       <Navbar />
+
+      <Footer />
     </>
   )
 }
