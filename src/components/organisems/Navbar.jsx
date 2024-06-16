d19208dca8f0c11f3b83f07ee23d2797d13f0939
rizanoor <rizanoorfauzan123@gmail.com>
2024-06-17 00:07:41 +0700

    feat: link navbar to pages

--- src/components/organisems/Navbar.jsx
+++ src/components/organisems/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import user from "../../assets/images/user.svg";
 import cart from "../../assets/images/cart.svg";
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <>
             <nav className="custom-navbar navbar navbar-expand-md navbar-dark bg-dark" aria-label="Furni navigation bar">
                 <div className="container">
-                    <a className="navbar-brand" to="/">Furni<span>.</span></a>
+                    <Link to="/" className="navbar-brand">Furni<span>.</span></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni"
                         aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -15,19 +16,19 @@ export default function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarsFurni">
                         <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
                             <li className="nav-item">
-                                <a className="nav-link">
+                                <Link to="/" className="nav-link">
                                     <span>Home</span>
-                                </a>
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link">
+                                <Link to="/shop" className="nav-link">
                                     <span>Shop</span>
-                                </a>
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link">
+                                <Link to="/services" className="nav-link">
                                     <span>Services</span>
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                         <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
@@ -38,9 +39,9 @@ export default function Navbar() {
                             </li>
                             
                             <li>
-                                <a className="nav-link">
+                                <Link to="/cart" className="nav-link">
                                     <img src={cart} alt="Cart" />
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
